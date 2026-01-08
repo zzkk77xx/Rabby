b73f8275e4b3452797d3a536db107a504f1afa8e
Naaate <alexandre.dupin@dusa.io>
2026-01-08 14:26:07 +0100

    replace rabby chrome web store id by multisub one

--- src/ui/utils/webapi.tsx
+++ src/ui/utils/webapi.tsx
@@ -62,12 +62,12 @@ export const openInternalPageInTab = (
 
 export const TRUSTED_URLS = {
   chromeStoreUrl:
-    'https://chromewebstore.google.com/detail/rabby-wallet/acmacodkjbdgmoleebolmdjonilkdbch',
+    'https://chromewebstore.google.com/detail/rabby-wallet/jlfkkgkmpijpelhnghimpolhbmonanop',
   chromeStoreReviewsUrl:
-    'https://chromewebstore.google.com/detail/rabby-wallet/acmacodkjbdgmoleebolmdjonilkdbch/reviews',
+    'https://chromewebstore.google.com/detail/rabby-wallet/jlfkkgkmpijpelhnghimpolhbmonanop/reviews',
   // // notice, only logged google user can access this page
   // chromeStoreMyReviewUrl:
-  //   'https://chromewebstore.google.com/detail/rabby-wallet/acmacodkjbdgmoleebolmdjonilkdbch/reviews/my-review',
+  //   'https://chromewebstore.google.com/detail/rabby-wallet/jlfkkgkmpijpelhnghimpolhbmonanop/reviews/my-review',
 };
 export const openTrustedExternalWebsiteInTab = async (
   type: keyof typeof TRUSTED_URLS,
